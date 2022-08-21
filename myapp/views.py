11e6b1cbd1bf17d323d59c19ee4c4d6da9b051ef
arunp007 <arunp4624@gmail.com>
2022-08-21 21:34:31 +0530

    Added some changes

--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,89 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def vishu(request):
     return render(request,'vishu.html')
 
 def astro(request):
     return render(request,'future.html')
+
+def ashwathi(request):
+    return render(request, 'ashwathi.html')
+
+def bharani(request):
+    return render(request, 'bharani.html')
+
+def karthika(request):
+    return render(request, 'karthika.html')
+
+def rohini(request):
+    return render(request, 'rohini.html')
+
+def magayiram(request):
+    return render(request, 'magayiram.html')
+
+def thiruvathira(request):
+    return render(request, 'thiruvathira.html')
+
+def punardam(request):
+    return render(request, 'punardam.html')
+
+def pooyam(request):
+    return render(request, 'pooyam.html')
+
+def aayilyam(request):
+    return render(request, 'aayilyam.html')
+
+def magham(request):
+    return render(request, 'magham.html')
+
+def pooram(request):
+    return render(request, 'pooram.html')
+
+def uthram(request):
+    return render(request, 'uthram.html')
+
+def atham(request):
+    return render(request, 'atham.html')
+
+def chithira(request):
+    return render(request, 'chithira.html')
+
+def chothi(request):
+    return render(request, 'chothi.html')
+
+def vishagham(request):
+    return render(request, 'vishagham.html')
+
+def anizham(request):
+    return render(request, 'anizham.html')
+
+def threketta(request):
+    return render(request, 'threketta.html')
+
+def moolam(request):
+    return render(request, 'moolam.html')
+
+def pooradam(request):
+    return render(request, 'pooradam.html')
+
+def uthradam(request):
+    return render(request, 'uthradam.html')
+
+def thiruvonam(request):
+    return render(request, 'thiruvonam.html')
+
+def avittam(request):
+    return render(request, 'avittam.html')
+
+def chadayam(request):
+    return render(request, 'chadayam.html')
+
+def puruttathi(request):
+    return render(request, 'puruttathi.html')
+
+def uthrattathi(request):
+    return render(request, 'uthrattathi.html')
+
+def revathi(request):
+    return render(request, 'revathi.html')
 
